f7bf9651a494d847d339a85436b89c70bb040f90
D4NILO07 <161032185+D4NILO07@users.noreply.github.com>
2024-03-15 10:41:01 -0300

    Add files via upload

---
+++ teste 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	//Declaração variáveis.
+	char nome[200] = "Marta"; // Cadeia
+	char sexo = 'F'; // Caracter
+	int idade = 20; // Inteiro
+	float peso = 58.400; // Real
+	
+	//Exibindo resultados.
+	printf("\n=== Exibindo resultados ===\n");
+	printf("Nome: %s \n", nome); // %s -> string -> cadeia
+	printf("Sexo: %c \n", sexo); // %c -> char -> caracter
+	printf("Idade: %i \n", idade); // %i -> into -> inteiro
+	printf("Peso: %f \n", peso); // %f -> float -> real
+	
+	return 0;
+}
---
+++ teste.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+// std -> standart
+// i -> input
+// o -> output
+
+// Função principal.
+int main(){
+	
+	// Comando para mostrar no terminal.
+	printf("Bem-Vindo Danilo!");
+	printf("\nYou are is beautifull!");
+	
+	// Comando para pressionar uma tecla
+	// antes de finalizar o programa.
+	getchar();
+	
+	//Comando para retornat o código 0.
+	//Código usado para informar que o
+	// programa finalizou normalmente.
+	return 0;
+}
+
+
